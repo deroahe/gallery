10aeb6ebaf4a03b619dd3591ac2e5f36c73df527
Bogdan Basag <deroahe@gmail.com>
2021-08-28 02:15:09 +0300

    Frontend ready

--- frontend/src/services/export.service.js
+++ frontend/src/services/export.service.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-// const API_URL_UTIL = "https://deroahe-gallery-backend.herokuapp.com/api/util";
-const API_URL_UTIL = "http://localhost:8080/api/util";
+const API_URL_UTIL = "https://deroahe-gallery-backend.herokuapp.com/api/util";
+// const API_URL_UTIL = "http://localhost:8080/api/util";
 
 
 const exportExcel = () => {
