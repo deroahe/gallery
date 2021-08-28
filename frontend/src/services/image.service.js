bc9a06eaf6012e990451548eae7651329963b9e7
Bogdan Basag <deroahe@gmail.com>
2021-08-28 18:24:25 +0300

    'My uploads' implemented on frontend

--- frontend/src/services/image.service.js
+++ frontend/src/services/image.service.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-// const API_URL = "https://deroahe-gallery-backend.herokuapp.com/api/images";
-const API_URL = "http://localhost:8080/api/images";
+const API_URL = "https://deroahe-gallery-backend.herokuapp.com/api/images";
+// const API_URL = "http://localhost:8080/api/images";
 
 const getImageById = (imageId) => {
     return axios.get(API_URL + "/" + imageId);
--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-// const API_URL_TEST = "https://deroahe-gallery-backend.herokuapp.com/api/test/";
-// const API_URL_USERS = "https://deroahe-gallery-backend.herokuapp.com/api/users";
-const API_URL_TEST = "http://localhost:8080/api/test/";
-const API_URL_USERS = "http://localhost:8080/api/users";
+const API_URL_TEST = "https://deroahe-gallery-backend.herokuapp.com/api/test/";
+const API_URL_USERS = "https://deroahe-gallery-backend.herokuapp.com/api/users";
+// const API_URL_TEST = "http://localhost:8080/api/test/";
+// const API_URL_USERS = "http://localhost:8080/api/users";
 
 const getPublicContent = () => {
     return axios.get(API_URL_TEST + "all");
